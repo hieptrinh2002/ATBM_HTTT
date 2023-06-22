8b62d9bc965c1e74d4ce102235a49909c0f45965
NGUYEN NHAT TRUONG <103358975+Ericc1909@users.noreply.github.com>
2023-06-22 23:09:01 +0700

    Create DAC_RBAC_VPD_Truong.sql

---
+++ Database/DAC_RBAC_VPD_Truong.sql
@@ -0,0 +1,304 @@
+
+-- tạo ADMIN
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+/
+--Tao user dba QLTGDA (SYS)
+CREATE USER QLTGDA IDENTIFIED BY admin;
+GRANT ALL PRIVILEGES TO QLTGDA WITH ADMIN OPTION;
+
+
+
+GRANT DBA TO QLTGDA;
+GRANT CREATE ROLE TO QLTGDA WITH ADMIN OPTION;
+GRANT ALTER USER TO QLTGDA WITH ADMIN OPTION;
+GRANT CREATE USER TO QLTGDA WITH ADMIN OPTION;
+GRANT ALTER ANY ROLE TO QLTGDA WITH ADMIN OPTION;
+GRANT DROP USER TO QLTGDA WITH ADMIN OPTION;
+GRANT DROP ANY ROLE TO QLTGDA WITH ADMIN OPTION;
+GRANT SELECT ANY DICTIONARY TO QLTGDA WITH ADMIN OPTION;
+GRANT CREATE ANY VIEW TO QLTGDA WITH ADMIN OPTION;
+
+
+GRANT EXECUTE ON DBMS_RLS TO QLTGDA;
+GRANT EXECUTE ON DBMS_CRYPTO TO QLTGDA;
+GRANT EXECUTE ON dbms_fga TO QLTGDA;
+GRANT UNLIMITED TABLESPACE TO QLTGDA;
+GRANT CREATE SESSION TO QLTGDA;
+/
+--Cap quyen vao bang DBMS_CRYPTO de cai dat ma hoa
+GRANT EXECUTE ON SYS.DBMS_CRYPTO TO QLTGDA;
+/
+--Cap quyen doc audit_trail
+GRANT SELECT ON DBA_AUDIT_TRAIL TO QLTGDA;
+/
+-- tao role
+create role SYSADMIN;
+GRANT SYSADMIN TO QLTGDA;
+
+CREATE ROLE NHANVIEN;
+/
+CREATE ROLE QLTRUCTIEP;
+/
+CREATE ROLE TRUONGPHONG;
+/
+CREATE ROLE TAICHINH;
+/
+CREATE ROLE NHANSU;
+/
+CREATE ROLE TRUONGDA;
+/
+
+--CS#1: NHAN VIEN 
+
+CREATE OR REPLACE VIEW VIEW_NHANVIEN_XEMTHONGTIN_CANHAN
+AS
+    SELECT *
+    FROM NHANVIEN
+    WHERE MANV = SYS_CONTEXT('USERENV', 'SESSION_USER');
+/
+CREATE OR REPLACE VIEW VIEW_NHANVIEN_XEM_THONGTIN_PHONGBAN
+AS
+    SELECT * 
+    FROM PHONGBAN
+/
+CREATE OR REPLACE VIEW VIEW_NHANVIEN_XEM_THONGTIN_DEAN
+AS
+    SELECT * 
+    FROM DEAN
+/
+CREATE OR REPLACE VIEW VIEW_TRUONGPHONG_THAOTAC_PHANCONG
+ AS
+    SELECT PC.* FROM NHANVIEN NV 
+    JOIN PHANCONG PC ON NV.MANV = PC.MANV
+    WHERE PHG IN (SELECT PHG FROM NHANVIEN WHERE MANV = SYS_CONTEXT('USERENV', 'SESSION_USER'));
+/
+
+--GRANT QUYEN ROLE NHANVIEN
+
+GRANT SELECT, UPDATE (NGAYSINH,DIACHI,SODT) ON VIEW_NHANVIEN_XEMTHONGTIN_CANHAN TO NHANVIEN;
+/
+GRANT SELECT ON VIEW_NHANVIEN_XEM_THONGTIN_PHONGBAN TO NHANVIEN;
+/
+GRANT SELECT ON VIEW_NHANVIEN_XEM_THONGTIN_DEAN TO NHANVIEN;
+/
+GRANT SELECT ON PHANCONG TO NHANVIEN; --(CAN CAI DAT VPD_1) 
+
+--VPD 1: Nhân viên có quyền xem tất cả các thuộc tính trên quan hệ PHANCONG liên quan đến chính nhân viên đó.
+
+
+CREATE OR REPLACE FUNCTION NHANVIEN_XEM_THONGTIN_PHANCONG
+    (schema_name IN VARCHAR2, table_name IN VARCHAR2)
+    RETURN VARCHAR2
+IS
+    predicate VARCHAR2(4000);
+BEGIN
+    predicate := 'MANV = SYS_CONTEXT(''USERENV'', ''SESSION_USER'')';
+
+    RETURN predicate;
+END;
+/
+
+
+BEGIN
+    DBMS_RLS.ADD_POLICY(
+        object_schema   => 'QLTGDA',
+        object_name     => 'NHANVIEN',
+        policy_name     => 'VPD_NHANVIEN_XEM_THONGTIN_PHANCONG',
+        function_schema => 'QLTGDA',
+        policy_function => 'NHANVIEN_XEM_THONGTIN_PHANCONG',
+        statement_types => 'SELECT',
+        update_check    => FALSE,
+        enable          => TRUE
+    );
+END;
+/
+
+BEGIN
+    DBMS_RLS.ADD_POLICY(
+        object_schema   => 'QLTGDA',
+        object_name     => 'PHANCONG',
+        policy_name     => 'VPD_NHANVIEN_XEM_THONGTIN_PHANCONG',
+        function_schema => 'QLTGDA',
+        policy_function => 'NHANVIEN_XEM_THONGTIN_PHANCONG',
+        statement_types => 'SELECT',
+        update_check    => FALSE,
+        enable          => TRUE
+    );
+END;
+/
+
+
+
+--CS#2: QL TRUC TIEP
+/
+GRANT NHANVIEN TO TRUONGPHONG;
+/
+GRANT SELECT ON NHANVIEN TO QLTRUCTIEP; --(CAN CAI DAT VPD_2)
+
+--VPD_ 2: QL TRUC TIEP CO THE XEM CAC THUOC TINH CUA NHAN VIEN MA MINH QUAN LY (NGOAI TRU LUONG VA PHUCAP)
+CREATE OR REPLACE FUNCTION QLTRUCTIEP_VPD_POLICY
+    (schema_name IN VARCHAR2, table_name IN VARCHAR2)
+    RETURN VARCHAR2
+IS
+    predicate VARCHAR2(4000);
+BEGIN
+    predicate := 'VAITRO = ''QL trực tiếp'' OR (MANQL = (SELECT MANV FROM NHANVIEN WHERE MANV = SYS_CONTEXT(''USERENV'',''SESSION_USER'')) AND COLUMN_NAME NOT IN (''LUONG'', ''PHUCAP''))';
+
+    RETURN predicate;
+END;
+/
+
+
+BEGIN
+    DBMS_RLS.ADD_POLICY(
+        object_schema   => 'QLTGDA',
+        object_name     => 'NHANVIEN',
+        policy_name     => 'VPD_QLTRUCTIEP_POLICY',
+        function_schema => 'QLTGDA',
+        policy_function => 'QLTRUCTIEP_VPD_POLICY',
+        statement_types => 'SELECT',
+        update_check    => FALSE,
+        enable          => TRUE
+    );
+END;
+/
+
+-- QUAN LY TRUC TIEP có thể xem các dòng trong quan hệ PHANCONG liên quan đến chính họ và các nhân viên được họ quản lý trực tiếp
+ --GRANT SELECT ON PHANCONG TO QLTRUCTIEP;(đã kế thừa từ NHANVIEN) --(CAN CAI DAT VPD_1)
+ CREATE OR REPLACE FUNCTION QLTRUCTIEP_PHANCONG
+    (schema_name IN VARCHAR2, table_name IN VARCHAR2)
+    RETURN VARCHAR2
+IS
+    predicate VARCHAR2(4000);
+BEGIN
+    predicate := 'VAITRO = ''QL trực tiếp'' OR (MANQL = (SELECT MANV FROM NHANVIEN WHERE MANV = SYS_CONTEXT(''USERENV'',''SESSION_USER'')) AND (MANV = :MANV OR EXISTS (SELECT 1 FROM PHANCONG WHERE MANV = :MANV AND MADA = NHANVIEN.MANV)))';
+
+    RETURN predicate;
+END;
+/
+
+
+BEGIN
+    DBMS_RLS.ADD_POLICY(
+        object_schema   => 'QLTGDA',
+        object_name     => 'PHANCONG',
+        policy_name     => 'VPD_QLTRUCTIEP_PHANCONG',
+        function_schema => 'QLTGDA',
+        policy_function => 'QLTRUCTIEP_PHANCONG',
+        statement_types => 'SELECT',
+        update_check    => FALSE,
+        enable          => TRUE
+    );
+END;
+/
+
+
+
+--CS#3: TRUONG PHONG
+/
+CREATE OR REPLACE VIEW VIEW_TRUONGPHONG_THONGTIN_PHANCONG 
+AS
+    SELECT PC.*
+    FROM NHANVIEN NV 
+    JOIN PHANCONG PC ON NV.MANV = PC.MANV
+    WHERE PHG IN (SELECT PHG FROM NHANVIEN WHERE MANV = SYS_CONTEXT('USERENV', 'SESSION_USER'));
+
+/
+GRANT NHANVIEN TO TRUONGPHONG; 
+/
+GRANT SELECT, UPDATE, DELETE ON VIEW_TRUONGPHONG_THONGTIN_PHANCONG TO TRUONGPHONG;
+/
+GRANT SELECT ON NHANVIEN TO TRUONGPHONG; --(CAN CAI DAT VPD_2)
+/
+-- TRUONG PHONG CO THE XEM CAC THUOC TINH CUA NHAN VIEN PHONG MINH (NGOAI TRU LUONG VA PHUCAP)
+
+CREATE OR REPLACE FUNCTION TRUONGPHONG_VPD_POLICY
+    (schema_name IN VARCHAR2, table_name IN VARCHAR2)
+    RETURN VARCHAR2
+IS
+    predicate VARCHAR2(4000);
+BEGIN
+    predicate := 'VAITRO = ''Trưởng phòng'' OR (MANQL = (SELECT MANV FROM NHANVIEN WHERE MANV = SYS_CONTEXT(''USERENV'',''SESSION_USER'')) AND COLUMN_NAME NOT IN (''LUONG'', ''PHUCAP''))';
+
+    RETURN predicate;
+END;
+/
+
+
+BEGIN
+    DBMS_RLS.ADD_POLICY(
+        object_schema   => 'QLTGDA',
+        object_name     => 'NHANVIEN',
+        policy_name     => 'VPD_TRUONGPHONG_POLICY',
+        function_schema => 'QLTGDA',
+        policy_function => 'TRUONGPHONG_VPD_POLICY',
+        statement_types => 'SELECT',
+        update_check    => FALSE,
+        enable          => TRUE
+    );
+END;
+/
+--CS#4: TAI CHINH
+GRANT NHANVIEN TO TAICHINH; 
+  -- TẠO TRIGGER XỬ LÍ CHỔ CHỈ UPDATE LUONG NV - GIAM DOC
+/
+
+--CS#5: NhanSu
+
+GRANT NHANVIEN TO NHANSU;
+/
+GRANT INSERT, UPDATE ON PHONGBAN TO NHANSU;
+/   
+GRANT UPDATE (MANV,TENNV,PHAI,NGAYSINH,DIACHI,SODT,VAITRO,MANQL,PHG) ON NHANVIEN TO NHANSU;
+    -- cài đặt proc để insert, update The Nhan vien với lương và phụ cấp có gt là NULL 
+-- CAI VPD: Nhan Su không được xem LUONG, PHUCAP của người khác và không được cập nhật trên các trường LUONG, PHUCAP.
+
+CREATE OR REPLACE FUNCTION NHANSU_VPD_POLICY
+    (schema_name IN VARCHAR2, table_name IN VARCHAR2)
+    RETURN VARCHAR2
+IS
+    predicate VARCHAR2(4000);
+BEGIN
+    predicate := 'VAITRO <> ''Nhân sự'' OR (VAITRO = ''Nhân sự'' AND SYS_CONTEXT(''USERENV'',''SESSION_USER'') = MANV)';
+
+    IF table_name = 'NHANVIEN' THEN
+        predicate := predicate || ' AND (COLUMN_NAME <> ''LUONG'' AND COLUMN_NAME <> ''PHUCAP'')';
+    END IF;
+
+    RETURN predicate;
+END;
+/
+
+BEGIN
+    DBMS_RLS.ADD_POLICY(
+        object_schema   => 'QLTGDA',
+        object_name     => 'NHANVIEN',
+        policy_name     => 'VPD_NHANSU_POLICY',
+        function_schema => 'QLTGDA',
+        policy_function => 'NHANSU_VPD_POLICY',
+        statement_types => 'SELECT, UPDATE',
+        update_check    => TRUE,
+        enable          => TRUE
+    );
+END;
+/
+
+
+
+
+
+--CS#6: TRUONG DE AN
+CREATE OR REPLACE VIEW VIEW_HIENTHI_ALL_DEAN
+AS  
+    SELECT * FROM DEAN;
+/
+GRANT NHANVIEN TO TRUONGDA;
+/
+GRANT INSERT, UPDATE, DELETE ON VIEW_HIENTHI_ALL_DEAN TO TRUONGDA;
+
+
+
+--CS#7: GIAM DOC ( KO CAI DAT )
+
+
+
